bea44041543eeb4a63fcfeeb67aabc83199d146c
k4zuk0 <anthony.moyart@gmail.com>
2021-10-08 22:55:36 +0200

    rework the title management to match nextjs' best practices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import '../styles/global.scss'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <>
+        <Head>
+            <title>Anthony Moyart - Développeur backend / web</title>
+        </Head>
+        <Component {...pageProps} />
+    </>
 }
