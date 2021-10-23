2dcd66c4fa7e25afc91ff7caa66b0eb9b86f61e7
anthony-moyart <11043713+anthony-moyart@users.noreply.github.com>
2021-10-23 21:41:05 +0200

    setup some basic mobile responsiveness

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ export default function App({ Component, pageProps }) {
     return <>
         <Head>
             <title>Anthony Moyart - Développeur backend / web</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
         <Component {...pageProps} />
     </>
